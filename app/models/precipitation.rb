d78acd7eb18c762c42a846c4a8e8a8c6e7fa9d3a
Horacio Alonso <horo@Valhalla>
2013-12-20 15:55:16 -0300

    import and model validation

--- app/models/precipitation.rb
+++ app/models/precipitation.rb
@@ -1,3 +1,9 @@
 class Precipitation < ActiveRecord::Base
-  
+  validates :date, uniqueness: { scope: :location, message: "Este dato ya fue creado" }
+
+  def self.import
+    SMN::Report.today_precipitations.measurements.each do |p|
+      Precipitation.create({:date => p.date, :mm => p.mm, :kind => p.type, :province => p.province, :location => p.location})
+    end  
+  end
 end
--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -1,3 +1,9 @@
 class Temperature < ActiveRecord::Base
-    
+  validates :date, uniqueness: { scope: :location, message: "Este dato ya fue creado" }
+
+  def self.import
+    SMN::Report.today_temperatures.measurements.each do |t|
+      Temperature.create({:date => t.date, :max => t.max, :min => t.min, :province => t.province, :location => t.location})
+    end
+  end    
 end
